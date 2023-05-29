194b786eaa21bfe75f178e25d81cfe8291ad1055
toavinathedev_luffy <toavinathefirst@gmail.com>
2023-05-29 11:47:03 +0300

    tutoriel sur le doc officiel debut

--- index.js
+++ index.js
@@ -1,34 +1,8 @@
-
-const electron= require("electron");
-const app=electron.app;
-const browseWindow=electron.BrowserWindow;
-const path =require("path");
-const url = require("url");
-let win;
-function createWindow(){
-    win = new electron.BrowserWindow();
-    win.loadURL(url.format({
-        pathname: path.join(__dirname,'index.html'),
-        protocol:'file',
-        slashes:true
-    }));
-    win.webContents.openDevTools();
-    win.on('closed',() => {
-        win=null;
+const {app,BrowserWindow} = require('electron');
+const createWindow =() => {
+    const win = new BrowserWindow({
+        width:800,height:600
     });
-
+    win.loadFile('index.html');
 }
-app.on('ready',createWindow);
-app.on('window-all-closed',()=>{
-    if(process.platform !== 'darwin') {
-        app.quit;
-    }
-});
-app.on('activate',()=>{
-    if(win===null){
-        createWindow();
-    }
-});
-
-
-
+app.createWindow;
